041a75059e56cc1e667e29a49f547a96a0345ca3
Morgane Vidal <imala34@hotmail.fr>
2015-11-24 21:35:51 +0100

    Correction addShelter

--- controller/addShelter.php
+++ controller/addShelter.php
@@ -2,7 +2,7 @@
 
 require_once 'models/Shelter.php';
 
-$addResult = Shelter::addShelter($_POST['name'], $_POST['phone'], $_POST['description'],
+$addResult = Shelter::addShelterInDataBase($_POST['name'], $_POST['phone'], $_POST['description'],
                                  $_POST['email'], $_POST['operationalHours'], $_POST['street'],
                                  $_POST['zipcode'], $_POST['city'], $_POST['latitude'], $_POST['longitude']);
 echo json_encode(['success' => $addResult]);
--- models/Address.php
+++ models/Address.php
@@ -11,10 +11,10 @@ class Address {
   public static function addAddress($street, $zipcode, $city, $latitude, $longitude) {
     $db = DbManager::getPDO();
     $query = "INSERT INTO Address(street, zipcode, city, latitude, longitude)
-              VALUES ('".$street."','".$zipcode."','".$city."','".$latitude."','".$longitude."')";
+              VALUES ('".$street."',".intval($zipcode).",'".$city."','".$latitude."','".$longitude."')";
     $db->exec($query);
 
-    $query = "SELECT idAddress FROM Address WHERE street = '".$street."', zipcode = '".$zipcode."', city = '".$city."';";
+    $query = "SELECT idAddress FROM Address WHERE street = '".$street."' AND zipcode = ".intval($zipcode)." AND city = '".$city."';";
     $res = $db->query($query)->fetch();
     return $res['idAddress'];
   }
