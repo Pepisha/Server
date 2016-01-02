7245ac05e3fd455ef67f40dff524e150ffe879a2
Morgane Vidal <imala34@hotmail.fr>
2016-01-02 09:47:38 +0100

    correction de la fonctionalité getShelterdAdoptedAnimals

--- controller/getSheltersAdoptedAnimals.php
+++ controller/getSheltersAdoptedAnimals.php
@@ -6,6 +6,6 @@ require_once 'models/Shelter.php';
 $user = new User($_POST['nickname']);
 $shelter = new Shelter($_POST['idShelter']);
 $listAdoptedAnimals = $shelter->getAdoptedAnimals();
-$listAnimals = $user->setFollowedAnimals($listAnimals);
+$listAnimals = $user->setFollowedAnimals($listAdoptedAnimals);
 
-echo json_encode($listAdoptedAnimals);
+echo json_encode($listAnimals);
